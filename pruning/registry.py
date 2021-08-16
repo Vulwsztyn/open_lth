6498b94707ff670a837fd3c93367b37a129a87a9
Artur Mostowski <artur.mostowski@currency-one.com>
2021-08-17 00:24:29 +0200

    pruning | sparse_layerwisev -> sparse_local for backwards compatibility (#8)

--- pruning/registry.py
+++ pruning/registry.py
@@ -10,7 +10,7 @@ from foundations.hparams import PruningHparams
 from pruning import sparse_global
 from pruning import sparse_layerwise
 
-registered_strategies = {'sparse_global': sparse_global.Strategy, 'sparse_layerwise': sparse_layerwise.Strategy}
+registered_strategies = {'sparse_global': sparse_global.Strategy, 'sparse_local': sparse_layerwise.Strategy}
 
 
 def get(pruning_hparams: PruningHparams):
